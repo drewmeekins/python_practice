abb6d7e6b0fc1e5ba3ef7c20d74c04955095ad2b
drewmeekins <drew.a.meekins@gmail.com>
2023-04-24 19:12:58 -0400

    day 2 done
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/day_2.py
@@ -0,0 +1,9 @@
+my_name = input("What is your name?: ")
+print("Hello there",my_name)
+
+my_age = input("How old are you?: ")
+print(my_age,"? Wow you're really old")
+
+replit = input("Do you like replit?: ")
+print(replit, "is what we like to hear!")
+
