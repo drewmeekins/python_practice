b213f2271908e6077fa7edf5885ec04429b90026
drewmeekins <drew.a.meekins@gmail.com>
2023-04-24 19:26:16 -0400

    finished day 4
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_4.py
@@ -0,0 +1,6 @@
+my_name = input("What is your name?: ")
+my_age = input("What is your age?: ")
+enemy = input("Who is your enemy?: ")
+super_power = input("What is your super power?: ")
+
+print("Our story begins as", my_name, "at the young age of", my_age, "battled", enemy, "with the power of", super_power,". He was victorious in the battle and saved the world.")
