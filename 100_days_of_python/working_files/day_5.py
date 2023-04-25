4bbf35f0551335e8fab853d1d2574435f471d547
drewmeekins <drew.a.meekins@gmail.com>
2023-04-25 19:15:32 -0400

    finished day 5
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_5.py
+++ 100_days_of_python/working_files/day_5.py
@@ -3,3 +3,9 @@ if my_name == "Drew":
     print("Hello Master")
 else: 
     print("Goodbye loser")
+
+avenger = input("Which Avenger are you?")
+if avenger == "Ironman":
+    print("Welcome Mr. Stark")
+else:
+    print('Access denied')
