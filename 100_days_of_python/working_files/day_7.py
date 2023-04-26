c87b6c1b57ac39386a364d0c4bbb948b086b414c
drewmeekins <drew.a.meekins@gmail.com>
2023-04-26 18:43:20 -0400

    Nesting good to go
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_7.py
@@ -0,0 +1,13 @@
+tv_show = input("What's your favorite TV show? ")
+
+if tv_show == "The Wire":
+    print("Good taste")
+    fav_character = input("So who's your favorite character?")
+    if fav_character == "Omar":
+        print("Damn, RIP Omar")
+    else:
+        print("Eh, I guess")
+elif tv_show == "The Sopranos":
+    print("A GOAT show")
+else:
+    print("Get some help, man")
