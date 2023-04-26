773aede127571e7f662f9ce755bd4b47038642cf
drewmeekins <drew.a.meekins@gmail.com>
2023-04-26 17:55:12 -0400

    usernames and passwords working
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_6.py
@@ -0,0 +1,12 @@
+print("USER LOGIN")
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "phillydam" and password == "password": 
+    print("Welcome administrator!")
+elif username == "Drew" and password == "1234":
+    print("secret names only!")
+elif username == "Kara":
+    print("how do you know about this?")
+else:
+    print("Access denied!")
