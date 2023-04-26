c00cfdcdea0911bba0c12de6465599b4cb8fe4bc
drewmeekins <drew.a.meekins@gmail.com>
2023-04-26 18:55:12 -0400

    day 8 finished
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_8.py
@@ -0,0 +1,5 @@
+# positive affirmations
+name = input("What's your name?")
+goal = input("Do you have a goal you would like to share?")
+feeling = input("On a scale of 1- 10 how are you feeling today?")
+
