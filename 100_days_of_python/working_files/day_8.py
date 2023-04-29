d99b04c691c5341fbf1ed59eb4d6e1ce49758052
drewmeekins <drew.a.meekins@gmail.com>
2023-04-29 15:41:02 -0400

    casting setup
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_8.py
+++ 100_days_of_python/working_files/day_8.py
@@ -3,3 +3,4 @@ name = input("What's your name?")
 goal = input("Do you have a goal you would like to share?")
 feeling = input("On a scale of 1- 10 how are you feeling today?")
 
+print(f"Hello there {name}")
---
+++ 100_days_of_python/working_files/day_9.py
@@ -0,0 +1,6 @@
+my_score = int(input("Your score: "))
+
+if my_score > 100000:
+    print("Winner!")
+else:
+    print("Try again")
