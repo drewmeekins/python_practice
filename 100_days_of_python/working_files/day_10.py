ac104e0ab68dcb58bdc00728cef45025318057cb
drewmeekins <drew.a.meekins@gmail.com>
2023-05-01 18:52:23 -0400

    math is mathing
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_10.py
@@ -0,0 +1,21 @@
+addition = 5 + 5
+print(addition)
+
+subtraction = 5 - 3
+print(subtraction)
+
+multiplication = 5 * 5
+print(multiplication)
+
+division = 20 / 5
+print(division)
+
+squared =  5 ** 5
+print(squared)
+
+modulo = 30 % 4
+print(modulo)
+
+divisor = 15 // 4
+print(divisor)
+
