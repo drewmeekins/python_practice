de79f59fdf621cc1001471fbe92e07317cee8698
drewmeekins <drew.a.meekins@gmail.com>
2023-05-03 16:37:39 -0400

    day 10 finished
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_10.py
+++ 100_days_of_python/working_files/day_10.py
@@ -1,21 +1,29 @@
-addition = 5 + 5
-print(addition)
+# addition = 5 + 5
+# print(addition)
 
-subtraction = 5 - 3
-print(subtraction)
+# subtraction = 5 - 3
+# print(subtraction)
 
-multiplication = 5 * 5
-print(multiplication)
+# multiplication = 5 * 5
+# print(multiplication)
 
-division = 20 / 5
-print(division)
+# division = 20 / 5
+# print(division)
 
-squared =  5 ** 5
-print(squared)
+# squared =  5 ** 5
+# print(squared)
 
-modulo = 30 % 4
-print(modulo)
+# modulo = 30 % 4
+# print(modulo)
 
-divisor = 15 // 4
-print(divisor)
+# divisor = 15 // 4
+# print(divisor)
+
+
+my_bill = float(input("What was the bill?: "))
+number_of_people = int(input("How many people at the table?: "))
+answer = my_bill / number_of_people
+answer = round(answer, 2)
+
+print("You all owe", answer)
 
