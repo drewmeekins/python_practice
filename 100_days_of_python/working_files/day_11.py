24cdafc3d3a9bf525ac34f49843c0b57c9c0965d
drewmeekins <drew.a.meekins@gmail.com>
2023-05-03 16:56:16 -0400

    day 11 finished_
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_11.py
@@ -0,0 +1,9 @@
+# How many seconds in a year?
+seconds = 1
+minutes = seconds * 60
+hours = minutes * 60
+days = hours * 24
+years = days * 365
+
+seconds_in_a_year = years
+print(seconds_in_a_year)
