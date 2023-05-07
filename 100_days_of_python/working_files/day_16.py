35b95ace61b4359bab3048443ac1b7ce656bb047
drewmeekins <drew.a.meekins@gmail.com>
2023-05-07 11:32:37 -0400

    day 16 finished
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_16.py
+++ 100_days_of_python/working_files/day_16.py
@@ -1,7 +1,19 @@
 # while true loops
+# while True:
+#     print("this program is running smoothly")
+#     go_again = input("Go again?: ")
+#     if go_again == "no":
+#         break #breaks out of the loop
+# print("Too bad, I was having fun")
+
+# simple calculator
+counter = 0
 while True:
-    print("this program is running smoothly")
-    go_again = input("Go again?: ")
-    if go_again == "no":
+    answer = int(input("Enter a number: "))
+    print("Adding up the numbers!")
+    counter += answer
+    print(f"Current total is, {counter}")
+    add_another = input("Add another?: ")
+    if add_another == "no":
         break
-print("Too bad, I was having fun")
+print(f"Your grand total is {counter}. Goodbye!")
