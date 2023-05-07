99c3b6d5d5f6e6fad4864b2ec3b74b8c198f51c3
drewmeekins <drew.a.meekins@gmail.com>
2023-05-07 11:26:25 -0400

    basic while loop setup
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_16.py
@@ -0,0 +1,7 @@
+# while true loops
+while True:
+    print("this program is running smoothly")
+    go_again = input("Go again?: ")
+    if go_again == "no":
+        break
+print("Too bad, I was having fun")
