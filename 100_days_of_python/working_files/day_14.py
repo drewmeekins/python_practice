6044f64ff58f008646141620daeeff2c1d425b1d
drewmeekins <drew.a.meekins@gmail.com>
2023-05-07 10:57:04 -0400

    rock setup
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ 100_days_of_python/working_files/day_14.py
@@ -0,0 +1,19 @@
+# rock paper scissors game
+
+# Hides input from each player
+from getpass import getpass as input
+
+
+
+print("Let's play Rock Paper Scissors!")
+
+player_1_move = input("Player 1, select Rock, Paper, or Scissors as a move")
+player_2_move = input("Player 2, select Rock, Paper, or Scissors as a move")
+
+if player_1_move == "Rock" and player_2_move == "Rock":
+    print(f"Draw! Player 1 selected {player_1_move} and Player 2 selected {player_2_move}")
+elif player_1_move == "Rock" and player_2_move == "Scissors":
+    print(f"Player 1 Wins! Player 1 selected {player_1_move} and Player 2 selected {player_2_move}")
+else:
+    print(f"Player 2 Wins! Player 2 selected {player_2_move} and Player 2 selected {player_1_move}")
+
