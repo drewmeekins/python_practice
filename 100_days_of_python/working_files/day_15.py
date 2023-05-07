8f2a1fc3459e449a22155e4ee497f83178e062f3
drewmeekins <drew.a.meekins@gmail.com>
2023-05-07 11:18:23 -0400

    aniaml game done. day 15 finished
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_15.py
+++ 100_days_of_python/working_files/day_15.py
@@ -1,5 +1,30 @@
 # while loops
-counter = 0
-while counter < 10:
-    print(counter)
-    counter += 1
+# numbers
+# counter = 0
+# while counter < 10:
+#     print(counter)
+#     counter += 1
+
+# text
+# exit = ""
+# while exit != "yes":
+#     print("😂")
+#     exit = input("Exit?: ")
+
+# kids game
+# Ask kids what animal they want and print out that sound
+animal = ""
+exit = ""
+while exit != "yes":
+    animal = input("What animal do you like?: ")
+    if animal == "Dog":
+        print("Bark!")
+    elif animal == "Cat":
+        print("Meow")
+    elif animal == "Cow":
+        print("Moo")
+    elif animal == "Bird":
+        print("Chirp")
+    else:
+        print("I don't know what that is.")
+    exit = input("Exit?: ")
