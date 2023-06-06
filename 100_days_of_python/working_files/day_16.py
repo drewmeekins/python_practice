2f4a9a0b73baa224566068b1378753a6477c2409
drewmeekins <drew.a.meekins@gmail.com>
2023-06-06 18:13:35 -0400

    added lyric game
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- 100_days_of_python/working_files/day_16.py
+++ 100_days_of_python/working_files/day_16.py
@@ -7,13 +7,24 @@
 # print("Too bad, I was having fun")
 
 # simple calculator
-counter = 0
+# counter = 0
+# while True:
+#     answer = int(input("Enter a number: "))
+#     print("Adding up the numbers!")
+#     counter += answer
+#     print(f"Current total is, {counter}")
+#     add_another = input("Add another? (y/n): ")
+#     if add_another == "n":
+#         break
+# print(f"Your grand total is {counter}. Goodbye!")
+
+# Fill in the blank lyric maker
+word = ""
 while True:
-    answer = int(input("Enter a number: "))
-    print("Adding up the numbers!")
-    counter += answer
-    print(f"Current total is, {counter}")
-    add_another = input("Add another? (y/n): ")
-    if add_another == "n":
+    print("Only thing that changed is the tail number on the flight. I could touch down and take off the same _____")
+    lyric = input("Finish the lyrics: ")
+    if lyric != "night":
+        print("Wrong, try again.")
+    else:
+        print("That's right!")
         break
-print(f"Your grand total is {counter}. Goodbye!")
